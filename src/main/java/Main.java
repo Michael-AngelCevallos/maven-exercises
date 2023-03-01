8a09f585dc27738f462bce4793933e3ced0fac3b
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-02-28 22:36:52 -0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,37 @@
+
+import org.apache.commons.lang3.StringUtils;
+
+
+
+import java.util.Scanner;
+
+public class Main {
+
+//    private String isNumeric() {
+//        if ()
+//    }
+
+    public static void main(String[] args) {
+        Scanner userInput =  new Scanner(System.in);
+
+        System.out.print("\n\tEnter something to modify your input: \n\t>");
+        String userString = userInput.nextLine();
+
+        System.out.print("\n\n\tUser Input: ");
+        System.out.print("\n\t" + userString);
+        System.out.print("\n\n\tModified Inputs:");
+
+
+        if (StringUtils.isNumeric(userString)) {
+            System.out.print("\n\t.isNumeric: The input is a number");
+        } else {
+            System.out.print("\n\t.isNumeric: The input is not a string");
+        }
+        System.out.print("\n\t.swapCase:  " + StringUtils.swapCase(userString));
+        System.out.print("\n\t.reverse:  " + StringUtils.reverse(userString));
+        System.out.print("\n\t.deleteWhiteSpace:  " + StringUtils.deleteWhitespace(userString) + "\n");
+
+
+
+    }
+}
